84641abff7ba3172d3ea886195f022eef012be38
faiyez <smfnoor@mun.ca>
2024-07-20 23:31:19 -0230

    Added new rooms

--- A Java/locations.java
+++ A Java/locations.java
@@ -16,9 +16,9 @@ public class locations {
     }
     
     public static Node[][] setLocations(){
-        node[8][15].setText("FIR");
-        node[8][15].setTextPresent(true);
-        //node[8][15].setBackground(Color.YELLOW);
+        node[11][22].setText("FIR");
+        node[11][22].setTextPresent(true);
+        node[11][22].setBackground(Color.YELLOW);
         node[8][15].setBorder(null);
         node[8][15].setBorderPainted(false);
         node[8][15].setContentAreaFilled(false);
@@ -26,40 +26,36 @@ public class locations {
         /*
          * ROWS ON THE BOTTOM 
          */
-        node[8][15].setText("GUARANA");
-        node[8][15].setTextPresent(true);
-        node[8][15].setBackground(Color.YELLOW);
-        node[11][9].setText("FIR");
-        node[11][9].setTextPresent(true);
-        node[11][9].setBackground(Color.YELLOW);
-        node[11][22].setText("DOGWOOD");
+        node[9][22].setText("GUARANA");
+        node[9][22].setTextPresent(true);
+        node[9][22].setBackground(Color.YELLOW);
+        node[11][22].setText("FIR");
         node[11][22].setTextPresent(true);
         node[11][22].setBackground(Color.YELLOW);
-        node[11][22].setText("BUTTERCUP");
-        node[11][22].setTextPresent(true);
-        node[11][22].setBackground(Color.YELLOW);
+        node[25][22].setText("DOGWOOD");
+        node[25][22].setTextPresent(true);
+        node[25][22].setBackground(Color.YELLOW);
+        node[27][22].setText("BUTTERCUP");
+        node[27][22].setTextPresent(true);
+        node[27][22].setBackground(Color.YELLOW);
         node[11][15].setText("BLUEBELI");
         node[11][15].setTextPresent(true);
         node[11][15].setBackground(Color.YELLOW);
         /*
          * END OF ROWS ON THE BOTTOM END
          */
-        node[8][13].setText("HEMLOCK");
-        node[8][13].setTextPresent(true);
-        node[8][13].setBackground(Color.YELLOW);
-        node[7][13].setText("FIREWEED");
-        node[7][13].setTextPresent(true);
-        node[7][13].setBackground(Color.YELLOW);
-        node[9][13].setText("EUCALYPTUS");
-        node[9][13].setTextPresent(true);
-        node[9][13].setBackground(Color.YELLOW);
-        node[14][13].setText("ELDERBERRY");
-        node[14][13].setTextPresent(true);
-        node[14][13].setBackground(Color.BLUE);
-        node[11][12].setTextPresent(true);
-        node[11][12].setBackground(Color.BLUE);
-        node[11][11].setTextPresent(true);
-        node[11][11].setBackground(Color.BLUE);
+        node[8][22].setText("HEMLOCK");
+        node[8][22].setTextPresent(true);
+        node[8][22].setBackground(Color.YELLOW);
+        node[10][22].setText("FIREWEED");
+        node[10][22].setTextPresent(true);
+        node[10][22].setBackground(Color.YELLOW);
+        node[12][22].setText("EUCALYPTUS");
+        node[12][22].setTextPresent(true);
+        node[12][22].setBackground(Color.YELLOW);
+        node[14][22].setText("ELDERBERRY");
+        node[14][22].setTextPresent(true);
+        node[14][22].setBackground(Color.BLUE);
         node[6][22].setText("IVY");
         node[6][22].setTextPresent(true);
         node[6][22].setBackground(Color.YELLOW);
@@ -81,9 +77,9 @@ public class locations {
         node[7][10].setText("MAHOGANY");
         node[7][10].setTextPresent(true);
         node[7][10].setBackground(Color.YELLOW);
-        node[4][5].setText("NIP APALM");
-        node[4][5].setTextPresent(true);
-        node[4][5].setBackground(Color.YELLOW);
+        node[5][8].setText("NIP APALM");
+        node[5][8].setTextPresent(true);
+        node[5][8].setBackground(Color.YELLOW);
         /*
          * Start of TOP ROW
          */
@@ -102,9 +98,9 @@ public class locations {
         node[11][8].setText("ROCKROSE");
         node[11][8].setTextPresent(true);
         node[11][8].setBackground(Color.YELLOW);
-        node[15][8].setText("SCOTSPINE");
-        node[15][8].setTextPresent(true);
-        node[15][8].setBackground(Color.YELLOW);
+        // node[15][8].setText("SCOTSPINE");
+        // node[15][8].setTextPresent(true);
+        // node[15][8].setBackground(Color.YELLOW);
         node[27][8].setText("ALDER");
         node[27][8].setTextPresent(true);
         node[27][8].setBackground(Color.YELLOW);
@@ -133,58 +129,45 @@ public class locations {
         /*
          * End of TOP ROW
          */
-        node[17][3].setText("SPRUCE");
-        node[17][3].setTextPresent(true);
-        node[17][3].setBackground(Color.YELLOW);
-        node[16][13].setText("COTTONWOOD");
-        node[16][13].setTextPresent(true);
-        node[16][13].setBackground(Color.BLUE);
-        node[16][11].setText("CYATHEA");
-        node[16][11].setTextPresent(true);
-        node[16][11].setBackground(Color.BLUE);
-        node[19][13].setText("CLOVER");
-        node[19][13].setTextPresent(true);
-        node[19][13].setBackground(Color.BLUE);
-        node[13][3].setText("SCOTS PINE");
-        node[13][3].setTextPresent(true);
-        node[13][3].setBackground(Color.BLUE);
-        node[14][3].setText("SPRUCE");
-        node[14][3].setTextPresent(true);
-        node[14][3].setBackground(Color.BLUE);
-        node[14][1].setText("SASSFRAS");
-        node[14][1].setTextPresent(true);
-        node[14][1].setBackground(Color.BLUE);
-        node[21][13].setText("CLOUDBERRY");
-        node[21][13].setTextPresent(true);
-        node[21][13].setBackground(Color.BLUE);
-        node[23][13].setText("CHICORY");
-        node[23][13].setTextPresent(true);
-        node[23][13].setBackground(Color.BLUE);
-        node[0][9].setText("DESK");
-        node[0][9].setTextPresent(true);
-        node[0][9].setBackground(Color.GREEN);
-        node[1][9].setText("SPACE");
-        node[1][9].setTextPresent(true);
-        node[1][9].setBackground(Color.GREEN);
-        node[1][10].setBackground(Color.GREEN);
-        node[0][10].setBackground(Color.GREEN);
-        node[1][11].setBackground(Color.GREEN);
-        node[0][11].setBackground(Color.GREEN);
+        node[16][22].setText("COTTONWOOD");
+        node[16][22].setTextPresent(true);
+        node[16][22].setBackground(Color.BLUE);
+        node[15][18].setText("CYATHEA");
+        node[15][18].setTextPresent(true);
+        node[15][18].setBackground(Color.BLUE);
+        node[19][22].setText("CLOVER");
+        node[19][22].setTextPresent(true);
+        node[19][22].setBackground(Color.BLUE);
+        node[16][8].setText("SCOTS PINE");
+        node[16][8].setTextPresent(true);
+        node[16][8].setBackground(Color.BLUE);
+        node[17][8].setText("SPRUCE");
+        node[17][8].setTextPresent(true);
+        node[17][8].setBackground(Color.BLUE);
+        node[15][2].setText("SASSFRAS");
+        node[15][2].setTextPresent(true);
+        node[15][2].setBackground(Color.BLUE);
+        node[21][22].setText("CLOUDBERRY");
+        node[21][22].setTextPresent(true);
+        node[21][22].setBackground(Color.BLUE);
+        node[23][22].setText("CHICORY");
+        node[23][22].setTextPresent(true);
+        node[23][22].setBackground(Color.BLUE);
         /*
          * Columns on the Right End
          */
-        node[27][9].setText("PINE");
-        node[27][9].setTextPresent(true);
-        node[27][9].setBackground(Color.YELLOW);
-        node[27][12].setText("CEDAR");
-        node[27][12].setTextPresent(true);
-        node[27][12].setBackground(Color.YELLOW);
-        node[27][15].setText("CHERRY");
-        node[27][15].setTextPresent(true);
-        node[27][15].setBackground(Color.YELLOW);
-        node[27][20].setText("MAPDE");
-        node[27][20].setTextPresent(true);
-        node[27][20].setBackground(Color.YELLOW);
+        node[28][9].setText("PINE");
+        node[28][9].setTextPresent(true);
+        node[28][9].setBackground(Color.YELLOW);
+        node[28][12].setText("CEDAR");
+        node[28][12].setTextPresent(true);
+        node[28][12].setBackground(Color.YELLOW);
+        node[28][15].setText("CHERRY");
+        node[28][15].setTextPresent(true);
+        node[28][15].setBackground(Color.YELLOW);
+        node[28][20].setText("MAPDE");
+        node[28][20].setTextPresent(true);
+        node[28][20].setBackground(Color.YELLOW);
 
         // Dimension size = new Dimension(20,20);
         // node[6][15].setPreferredSize(size);
